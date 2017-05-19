426d378646634272883f44e4cb373c4600c0f3ee
LordFabricio <ftlord@gmail.com>
2017-05-18 21:04:38 -0300

    Colei o que fizemos na aula. Agora é so incrementa.

--- src/Jogo.java
+++ src/Jogo.java
@@ -2,4 +2,79 @@
  * Created by ftlor on 18/05/2017.
  */
 public class Jogo {
+    String nome;
+    int vida;
+    int nivel;
+    int arma = 10;
+    int forca;
+    int destreza;
+    int energia;
+    int exp;
+    int mana;
+    int moedas;
+    int monstro1;
+    int monstro2;
+    int monstro3;
+    int chefe;
+    int forcaM1 = 5, forcaM2, forcaM3, forcaC;
+    int destrezaM1 = 10, destrezaM2, destrezaM3, destrezaC;
+    int vidaM1 = 100, vidaM2 = 100, vidaM3 = 100, vidaC = 100;
+
+    String dormir() {
+        String d = "";
+        if (energia <= 20) {
+            energia = 100;
+            moedas = moedas - 5;
+        } else {
+            d = ("Energia mбxima permitida");
+        }
+        return d;
+    }
+
+    String verificarStatus() {
+        String mensagem;
+        mensagem = ("Nome................: " + nome)
+                + ("\nNнvel.............: " + nivel)
+                + ("\nVida..............: " + vida)
+                + ("\n Durabilidade.....: " + arma)
+                + ("\nMana..............: " + mana)
+                + ("\nForзa.............: " + forca)
+                + ("\nExperiкncia.......: " + exp)
+                + ("\nEnergia...........: " + energia)
+                + ("\nDestreza..........: " + destreza)
+                + ("\nMoedas............: " + moedas);
+        return mensagem;
+    }
+
+    String upar() {
+        String p = "";
+        if (exp >= 100) {
+            nivel++;
+            exp = 0;
+            destreza = destreza + 10;
+            energia = 100;
+            forca = forca + 10;
+            mana = 100;
+        } else {
+            p = ("Vocк nгo pode subir de nнvel! Exp insuficiente");
+        }
+        return p;
+    }
+
+    String monstro1() {
+        String m = "";
+        if (forca > destrezaM1){
+            arma = arma - 1;
+            energia = energia - 20;
+            vidaM1 = vidaM1 - forca;
+            vida = vida  - forcaM1;
+        }else if (vidaM1 == 0){
+            m = ("Parabйns, vocк matou o Charmander");
+            exp = exp + 30;
+            moedas = moedas + 30;
+            vidaM1 = 100;
+        }
+        return m;
+    }
+
 }
--- src/JogoPrincipal.java
+++ src/JogoPrincipal.java
@@ -1,5 +1,40 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Created by ftlor on 18/05/2017.
  */
 public class JogoPrincipal {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        Jogo criar = new Jogo();
+        Random r = new Random();
+
+        System.out.println("Nome: ");
+        criar.nome = entrada.nextLine();
+        criar.nivel = 1;
+        criar.mana = 0;
+        criar.forca = 10;
+        criar.exp = 0;
+        criar.vida = 100;
+        criar.energia = 100;
+        criar.destreza = 10 + r.nextInt(20);
+
+        while (true) {
+            System.out.println("===== Skyrim Piorado =====");
+            System.out.println("1. Atacar");
+            System.out.println("2. Comprar");
+            System.out.println("3. Dormir");
+            System.out.println("4. Status");
+            System.out.println("5. Subir de Nнvel");
+            System.out.println("6. Sair");
+
+            int escolha = entrada.nextInt();
+            switch (escolha){
+                case 1:
+
+
+            }
+        }
+    }
 }
